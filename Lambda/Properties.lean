5607445f660ae49a541a07092f0a1bc348ff11cf
daniel <misternutel@gmail.com>
2025-06-09 22:29:17 -0500

    Theorem was changed to lemma in some results

--- Lambda/Properties.lean
+++ Lambda/Properties.lean
@@ -2,23 +2,23 @@ import Lambda.Defs
 import Mathlib.Tactic.Ring
 open Lambda
 
-theorem unshift_var_le {c i n : Nat} :
+lemma unshift_var_le {c i n : Nat} :
   c ≤ n → unshift c i (var n) = var (n - i) := by
   intro h
   unfold unshift
   have nnltc : ¬ (n < c) := by omega
   simp [nnltc]
 
-theorem unshift_var_lt {c i n : Nat} : n < c → (↓) c i n = var n := by
+lemma unshift_var_lt {c i n : Nat} : n < c → (↓) c i n = var n := by
   intro h; unfold unshift; simp [h]
 
-theorem shift_zero {N : Lambda} : ∀ {c}, (↑) c 0 N = N := by
+lemma shift_zero {N : Lambda} : ∀ {c}, (↑) c 0 N = N := by
   induction N with
   | var n => intro; simp
   | app N₁ N₂ ih₁ ih₂ => intro; simp [ih₁, ih₂]
   | abs M ih => intro; simp [ih]
 
-theorem shift_add' (N : Lambda) :
+lemma shift_add' (N : Lambda) :
   ∀ {c i₁ i₂ : Nat}, (↑) c i₁ ((↑) c i₂ N) = (↑) c (i₁ + i₂) N := by
   induction N with
   | var n => intro; repeat (first | omega | simp | split_ifs)
@@ -26,7 +26,7 @@ theorem shift_add' (N : Lambda) :
   | abs M ih => intro; simp [ih]
 
 @[simp]
-theorem shift_add (n m : Nat) (N : Lambda)
+lemma shift_add (n m : Nat) (N : Lambda)
   : ∀ (i j : Nat), i ≤ j → j ≤ i + m → (↑) j n ((↑) i m N) = (↑) i (m + n) N := by
   induction N with
   | var k => intros; repeat (first | omega | simp | split_ifs)
@@ -35,7 +35,7 @@ theorem shift_add (n m : Nat) (N : Lambda)
   | abs e ih => intros; simp; apply ih <;> omega
 
 @[simp]
-theorem subst_gt_range {M : Lambda} : ∀ {N n}, n > range M → M[n := N] = M := by
+lemma subst_gt_range {M : Lambda} : ∀ {N n}, n > range M → M[n := N] = M := by
   induction M with
   | var m =>
     intro _ _ h; unfold range at h
@@ -47,7 +47,7 @@ theorem subst_gt_range {M : Lambda} : ∀ {N n}, n > range M → M[n := N] = M :
     simp [ih1 ngt1, ih2 ngt2]
   | abs M ih => intro _ _ h; simp [subst, ih (Nat.lt_add_right 1 h)]
 
-theorem gt_range_gt_shift {N : Lambda} :
+lemma gt_range_gt_shift {N : Lambda} :
   ∀ {n i j}, n > range N → n + j > range ((↑) i j N) := by
   induction N with
   | var m => intros; simp_all; split_ifs <;> simp <;> omega
@@ -55,14 +55,15 @@ theorem gt_range_gt_shift {N : Lambda} :
   | abs N ih => intro _ _ _ h; simp [ih h]
 
 @[simp]
-theorem shift_unshift_id (M : Lambda) (c i : Nat) : (↓) (c + i) i ((↑) c i M) = M := by
+lemma shift_unshift_id (M : Lambda) (c i : Nat) : (↓) (c + i) i ((↑) c i M) = M := by
   induction M generalizing c with
   | var n => repeat (first | simp | split_ifs | omega)
   | app M₁ M₂ ih1 ih2 => simp [ih1, ih2]
   | abs M ih =>
     simp; rw [Nat.add_assoc, Nat.add_comm i 1, ← Nat.add_assoc]; exact ih (c + 1)
 
-theorem shift_subst  (M : Lambda) (k j i : Nat) (klej : k ≤ j) (L : Lambda) :
+@[simp]
+lemma shift_subst  (M : Lambda) (k j i : Nat) (klej : k ≤ j) (L : Lambda) :
   (↑) k i (M[j := L]) = ((↑) k i M)[j + i := L] := by
   induction M generalizing k j with
   | var n => repeat (first | simp | split_ifs | omega | apply shift_add)
@@ -76,7 +77,7 @@ theorem shift_subst  (M : Lambda) (k j i : Nat) (klej : k ≤ j) (L : Lambda) :
     exact Nat.add_le_add_right klej 1
 
 @[simp]
-theorem shift_subst_eq_shift (M N : Lambda) (k i j : Nat) :
+lemma shift_subst_eq_shift (M N : Lambda) (k i j : Nat) :
   k ≤ i → i < k + (j + 1) → (↑) k j M = ((↑) k (j + 1) M)[i := N] := by
   induction M generalizing k i with
   | var n => repeat (first | simp | split_ifs | omega)
@@ -100,18 +101,15 @@ theorem substitution (M N L : Lambda) (n m : Nat) (nlem : n ≤ m) :
   | var k =>
     rcases Nat.lt_trichotomy k n with klen | rfl |kgtn
     -- * Case k < n
-    . have n_gt_range_k : n > range k := klen
-      have m_gt_range_k : m > range k := Nat.lt_of_lt_of_le klen nlem
-      have m1_gt_range_k : m + 1 > range k := Nat.lt_add_right 1 m_gt_range_k
-      simp_all only [subst_gt_range]
+    . simp_all [Nat.lt_of_lt_of_le klen nlem, Nat.lt_add_right 1 (Nat.lt_of_lt_of_le klen nlem)]
     -- * Case k = n
-    . have k_le_m1 : k < m + 1 := by exact Nat.lt_add_one_of_le nlem
+    . have k_le_m1 : k < m + 1 := by omega
       simp [k_le_m1]
       apply Eq.symm
       have zero_le_mmk : 0 ≤ m - k := (Nat.le_sub_iff_add_le' nlem).mpr nlem
-      have := shift_subst N 0 (m -k) k zero_le_mmk L
-      have m_eq : m - k + k = m := Nat.sub_add_cancel nlem
-      rw [m_eq] at this
+      have := shift_subst N 0 (m - k) k zero_le_mmk L
+      rw [Nat.sub_add_cancel nlem] at this
+      simp at this
       assumption
     -- * Case k > n
     . have nklen : ¬ (k < n) := Nat.not_lt_of_gt kgtn
@@ -146,9 +144,7 @@ theorem substitution (M N L : Lambda) (n m : Nat) (nlem : n ≤ m) :
         rw [n_eq_km1] at this
         have : k - 1 ≠ k - 1 := Nat.ne_of_lt this
         contradiction
-  | app M₁ M₂ ih1 ih2 =>
-    simp
-    constructor <;> (first | apply ih1 | apply ih2) <;> assumption
+  | app M₁ M₂ ih1 ih2 => simp; constructor <;> (first | apply ih1 | apply ih2) <;> assumption
   | abs M ih =>
     have := ih (n + 1) (m + 1) (Nat.add_le_add_right nlem 1)
     rw [Nat.add_sub_add_right m 1 n] at this
